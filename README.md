a9bae084be0632554a8878e673294d331a0dd5aa
Christopher Adams <christopher@fabricatorz.com>
2015-10-01 18:23:00 +0800

    Generate supporters list on its own page

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 FREEBASSEL is a campaign to bring about the safe and immediate release of Bassel Khartabil from wrongful detainment in Syria since March 2012. He is a well known contributor to global software and culture communities like Creative Commons, Mozilla Firefox, Wikipedia, Open Clip Art Library, Fabricatorz, and Sharism. He is missed by these communities, his family, friends and loved ones. The campaign says, We will not stop campaigning for him until we see him as a free global citizen once again.
 
 [freebassel.org](http://freebassel.org)
+
+# Updating the supporters list
+
+If you have access to the Freebassel supporters list via Google Docs, you can update it with a script.
+
+## Requirements
+
+* Ruby
+* Bundler
+
+To install the necessary gems, run:
+
+  bundle install
+
+## Instructions
+
+Create a Google API client ID and secret, as per these instructions:
+
+  * https://developers.google.com/drive/web/auth/web-server
+
+Copy `config/config-sample.rb` to `config/config.rb` and fill in the Client ID, Secret, and the spreadsheet ID.
+
+Run the script with:
+
+  bundle exec ruby get_supporters_data.rb
+
+Follow the instructions to authorize the application. The `_data/supporters.json` will be updated.
---
+++ get_supporters_data.rb
@@ -0,0 +1,48 @@
+require "google/api_client"
+require "google_drive"
+
+require_relative "config/config.rb"
+
+# Authorize with OAuth and gets an access token
+client = Google::APIClient.new
+auth = client.authorization
+auth.client_id = GOOGLE_CLIENT_ID
+auth.client_secret = GOOGLE_CLIENT_SECRET
+
+auth.scope = [
+  "https://www.googleapis.com/auth/drive"
+]
+
+auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+
+# Prompt the user to authorize the app
+print("1. Open this page:\n%s\n\n" % auth.authorization_uri)
+print("2. Enter the authorization code shown in the page: ")
+auth.code = $stdin.gets.chomp
+auth.fetch_access_token!
+access_token = auth.access_token
+
+# Create a session
+session = GoogleDrive.login_with_oauth(access_token)
+
+# Get the worksheet
+ws = session.spreadsheet_by_key(GOOGLE_SPREADSHEET_KEY).worksheets[0]
+
+# Remove the first element, which is the sheet headers
+responses = ws.rows.drop(1)
+
+# Get relevant data for each supporter
+supporters = responses.map do |row|
+  {
+    :family_name => row[1],
+    :given_name => row[2],
+    :organization => row[3],
+    :city => row[8],
+    :country => row[9]
+  }
+end
+
+# Write the supporters list as JSON into the _data directory
+File.open("_data/supporters.json", "w") do |f|
+  f.write(JSON.pretty_generate(supporters))
+end
